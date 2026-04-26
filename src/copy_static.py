6901f67f2cf5eb7e94208fc2dd0b5440bbbd6765
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-04-26 20:44:02 +0530

    Add func() to copy static folder contents

---
+++ src/copy_static.py
@@ -0,0 +1,32 @@
+import shutil
+import os
+
+def copy_static():
+    source_dir = os.path.join(os.getcwd(), 'static')
+    destination_dir = os.path.join(os.getcwd(), 'public')
+
+    delete_dir_contents(destination_dir)
+
+    copy_contents(source_dir, destination_dir)
+
+
+def copy_contents(source_dir, destination_dir):
+    for item in os.listdir(source_dir):
+        source_path = os.path.join(source_dir, item)
+        destination_path = os.path.join(destination_dir, item)
+
+        if os.path.isfile(source_path):
+            shutil.copy2(source_path, destination_path)
+        else:
+            os.makedirs(destination_path, exist_ok=True)
+            copy_contents(source_path, destination_path)
+
+
+def delete_dir_contents(directory):
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        else:
+            shutil.rmtree(item_path)
